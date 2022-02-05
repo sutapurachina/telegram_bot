a63bcbec59b633aacf0e2639017c9c23c6a0d326
sutapurachina <98025538+sutapurachina@users.noreply.github.com>
2022-02-05 12:52:28 +0500

    Add files via upload

---
+++ test_project/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+//точка входа
+func main() {
+	botToken := "5230370682:AAHrXRY8nolFklbwpUGTPWCFVERSa0qx9No"
+	//https://api.telegram.org/bot<token>/METHOD_NAME
+	botApi := "https://api.telegram.org/bot"
+	botUrl := botApi + botToken
+	offset := 0
+	for {
+		updates, err := getUpdates(botUrl, offset)
+		if err != nil {
+			log.Println("smth went wrong: ", err.Error())
+		}
+		fmt.Println(updates)
+		for _, update := range updates {
+			err = respond(botUrl, update)
+			if err != nil {
+				log.Println("smth went wrong: ", err.Error())
+			}
+			offset = update.UpdateId + 1
+		}
+	}
+}
+
+// запрос обновлений
+func getUpdates(botUrl string, offset int) ([]Update, error) {
+	resp, err := http.Get(botUrl + "/getUpdates" + "?offset=" + strconv.Itoa(offset))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var restResponse RestResponse
+	err = json.Unmarshal(body, &restResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return restResponse.Result, nil
+}
+func respond(botUrl string, update Update) error {
+	var botMessage BotMessage
+	botMessage.ChatId = update.Message.Chat.ChatId
+	botMessage.Text = update.Message.Text
+	fmt.Println("a:", botMessage.Text)
+	buf, err := json.Marshal(botMessage)
+	if err != nil {
+		return err
+	}
+	_, err = http.Post(botUrl+"/sendMessage", "application/json", bytes.NewBuffer(buf))
+	if err != nil {
+		return err
+	}
+	return nil
+}
